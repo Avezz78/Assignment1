8538f7eaf8b8a4ceb30911ed9a800bafb68188e1
Avezz78 <113528051+Avezz78@users.noreply.github.com>
2022-09-27 20:58:13 +0530

    Add files via upload

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Prime numbers between 1 and 100 are: ");
+           
+            for (int i = 1; i < 101; i++)
+            {
+                count = 0;
+                if (i > 1)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            count = 1;
+                            break;
+                        }
+                    }
+                    if (count == 0)
+                    {
+                        Console.Write(i + "  ");
+                    }
+                }
+            }
+        }
+    }
+}
+            
+        
+    
+
---
+++ characterExist/characterExist/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace characterExist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "hello";
+            char value = 'a';
+            Boolean result = s.Contains(value);
+            Console.WriteLine(result);  
+        }
+    }
+}
