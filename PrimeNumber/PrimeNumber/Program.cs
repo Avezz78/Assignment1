39a712f32085ecdcdf85e6fb23402f6a7c51f799
Avezz78 <113528051+Avezz78@users.noreply.github.com>
2022-09-28 14:56:46 +0530

    Update Program.cs

--- PrimeNumber/PrimeNumber/Program.cs
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -6,23 +6,23 @@ namespace PrimeNumber
     {
         static void Main(string[] args)
         {
-            int count = 0;
+            int temp = 0;
             Console.WriteLine("Prime numbers between 1 and 100 are: ");
            
-            for (int i = 1; i < 101; i++)
+            for (int i = 1; i < 100; i++)
             {
-                count = 0;
+                temp = 0;
                 if (i > 1)
                 {
                     for (int j = 2; j < i; j++)
                     {
                         if (i % j == 0)
                         {
-                            count = 1;
+                            temp = 1;
                             break;
                         }
                     }
-                    if (count == 0)
+                    if (temp == 0)
                     {
                         Console.Write(i + "  ");
                     }
